33e2b126d6e7e5b82d52ab42c0fc16b0b9e8f583
NoobSaiyan <ms19961998@gmail.com>
2020-04-21 20:03:39 +0530

    Updates

---
+++ precache-manifest.1eb7f7849f12d72926658900e9b9c4c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddc9a108336f53510c6fa888c66f438",
+    "url": "/monster-robodex-react/index.html"
+  },
+  {
+    "revision": "06ea98c5156757fad3d3",
+    "url": "/monster-robodex-react/static/css/main.319427f3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-robodex-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-robodex-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ea98c5156757fad3d3",
+    "url": "/monster-robodex-react/static/js/main.f13d9bbc.chunk.js"
+  },
+  {
+    "revision": "17f05bc9e55c5c954931",
+    "url": "/monster-robodex-react/static/js/runtime-main.20784fc5.js"
+  }
+]);
